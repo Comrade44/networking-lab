2b89aad960e01627e37c5ac4a60d7b03922775e1
Richard McGinn <richard.mcginn@blakyaks.com>
2025-06-05 14:09:46 +0100

    Fix RG reference

--- terraform/uks-compa-net.tf
+++ terraform/uks-compa-net.tf
@@ -49,7 +49,7 @@ resource "azurerm_virtual_network_peering" "compa-uks-hub-uks" {
 
 resource "azurerm_virtual_network_peering" "hub-uks-compa-uks" {
   name                      = "hub-uks-compa-uks"
-  resource_group_name       = azurerm_resource_group.rg-uks-compa-net-01.name
+  resource_group_name       = azurerm_resource_group.rg-uks-hub-01.name
   virtual_network_name      = azurerm_virtual_network.vnet-uks-hub-01.name
   remote_virtual_network_id = azurerm_virtual_network.vnet-uks-compa-01.id
 }
