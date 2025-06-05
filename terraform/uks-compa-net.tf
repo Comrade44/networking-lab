da2bc47b1e392fb25682bf7268f7c8956d8949b7
Richard McGinn <richard.mcginn@blakyaks.com>
2025-06-05 11:46:32 +0100

    Add nsg

--- terraform/uks-compa-net.tf
+++ terraform/uks-compa-net.tf
@@ -16,3 +16,26 @@ resource "azurerm_subnet" "vnet-uks-compa-01-snet-01" {
   resource_group_name  = azurerm_resource_group.rg-uks-compa-net-01.name
   address_prefixes     = ["10.10.1.0/24"]
 }
+
+resource "azurerm_subnet_network_security_group_association" "nsg-uks-compa-01-snet-01" {
+  subnet_id                 = azurerm_subnet.vnet-uks-compa-01-snet-01.id
+  network_security_group_id = azurerm_network_security_group.nsg-uks-compa-01.id
+}
+
+resource "azurerm_network_security_group" "nsg-uks-compa-01" {
+  name                = "nsg-uks-compa-01"
+  resource_group_name = azurerm_resource_group.rg-uks-compa-net-01.name
+  location            = azurerm_resource_group.rg-uks-compa-net-01.location
+
+  security_rule = {
+    name                       = "allow-ssh"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
