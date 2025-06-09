efb777b507e382d728464ad90700c867d1ee439f
Richard McGinn <richard.mcginn@blakyaks.com>
2025-06-09 14:26:54 +0100

    Fix output value

--- terraform/outbound-nat.tf
+++ terraform/outbound-nat.tf
@@ -40,7 +40,7 @@ resource "azurerm_nat_gateway_public_ip_association" "ngw-01-pip-compa-01" {
 }
 
 resource "azurerm_subnet_nat_gateway_association" "comps-ngw-subnets" {
-  for_each = azurerm_virtual_network.vnet-uks-compa-01.subnets
+  for_each = azurerm_virtual_network.vnet-uks-compa-01.subnet
 
   subnet_id = each.value.subnet_id
   nat_gateway_id = azurerm_nat_gateway.ngw-uks-compa-01.id
