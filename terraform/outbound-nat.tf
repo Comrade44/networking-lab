2eed319455c39a4aefc4690a3996c314a658c696
Richard McGinn <richard.mcginn@blakyaks.com>
2025-06-09 14:39:22 +0100

    Fix for_each

--- terraform/outbound-nat.tf
+++ terraform/outbound-nat.tf
@@ -39,12 +39,12 @@ resource "azurerm_nat_gateway_public_ip_association" "ngw-01-pip-compa-01" {
   public_ip_address_id = azurerm_public_ip.pip-uks-compa-01.id
 }
 
-#resource "azurerm_subnet_nat_gateway_association" "comps-ngw-subnets" {
-#  for_each = azurerm_virtual_network.vnet-uks-compa-01.subnet
-#
-#  subnet_id = each.value.subnet_id
-#  nat_gateway_id = azurerm_nat_gateway.ngw-uks-compa-01.id
-#}
+resource "azurerm_subnet_nat_gateway_association" "comps-ngw-subnets" {
+  for_each = { for x in(azurerm_virtual_network.vnet-uks-compa-01.subnet) : x.name => x }
+
+  subnet_id      = each.value.id
+  nat_gateway_id = azurerm_nat_gateway.ngw-uks-compa-01.id
+}
 
 ## NAT gateway spoke b
 
