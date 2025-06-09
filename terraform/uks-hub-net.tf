e4d73377ec3fe658e35a6732f7b5ce98a98b1c45
Richard McGinn <richard.mcginn@blakyaks.com>
2025-06-09 10:54:26 +0100

    Add nat gateway and PIP to hub

--- terraform/uks-hub-net.tf
+++ terraform/uks-hub-net.tf
@@ -24,9 +24,22 @@ resource "azurerm_subnet" "bastion_subnet" {
   address_prefixes     = ["10.0.5.0/24"]
 }
 
+## NAT Gateway
 resource "azurerm_nat_gateway" "ngw-uks-hub-01" {
   name                = "ngw-uks-hub-01"
   location            = azurerm_resource_group.rg-uks-hub-01.location
   resource_group_name = azurerm_resource_group.rg-uks-hub-01.name
   sku_name            = "Standard"
+}
+
+resource "azurerm_public_ip" "pip-uks-hub-01" {
+  name                = "pip-uks-hub-01"
+  location            = azurerm_resource_group.rg-uks-hub-01.location
+  resource_group_name = azurerm_resource_group.rg-uks-hub-01.name
+  allocation_method   = "Static"
+}
+
+resource "azurerm_nat_gateway_public_ip_association" "ngw-01-pip-01" {
+  nat_gateway_id       = azurerm_nat_gateway.ngw-uks-hub-01.id
+  public_ip_address_id = azurerm_public_ip.pip-uks-hub-01.id
 }
