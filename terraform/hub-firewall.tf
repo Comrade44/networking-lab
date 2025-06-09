9638a4cdeaf32d3c1f4b63024a14e33a6bc5a9c4
Richard McGinn <richard.mcginn@blakyaks.com>
2025-06-09 16:44:28 +0100

    Add firewall management subnet

--- terraform/hub-firewall.tf
+++ terraform/hub-firewall.tf
@@ -5,6 +5,13 @@ resource "azurerm_subnet" "vnet-uks-hub-01-snet-azfw" {
   address_prefixes     = ["10.0.0.0/24"]
 }
 
+resource "azurerm_subnet" "vnet-uks-hub-01-snet-azfwmgmt" {
+  name                 = "AzureFirewallManagementSubnet"
+  resource_group_name  = azurerm_resource_group.rg-uks-hub-01.name
+  virtual_network_name = azurerm_virtual_network.vnet-uks-hub-01.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
 resource "azurerm_public_ip" "pip-uks-hub-01" {
   name                = "pip-uks-hub-01"
   location            = azurerm_resource_group.rg-uks-hub-01.location
@@ -20,7 +27,7 @@ resource "azurerm_firewall" "azfw-uks-hub-01" {
   sku_tier            = "Basic"
   ip_configuration {
     name                 = "hub"
-    subnet_id            = azurerm_subnet.vnet-uks-hub-01-snet-azfw.id
+    subnet_id            = azurerm_subnet.vnet-uks-hub-01-snet-azfwmgmt.id
     public_ip_address_id = azurerm_public_ip.pip-uks-hub-01.id
   }
   management_ip_configuration {
