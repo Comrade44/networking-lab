7fbfbbd8b858f9451c83c27eee9bd371d1518cdb
Richard McGinn <richard.mcginn@blakyaks.com>
2025-06-10 16:47:35 +0100

    Fix RG name

--- terraform/hub-firewall-nat-gw.tf
+++ terraform/hub-firewall-nat-gw.tf
@@ -1,7 +1,7 @@
 resource "azurerm_nat_gateway" "ngw-uks-hub-01" {
   name                = "ngw-uks-hub-01"
-  location            = azurerm_resource_group.rg-uks-hub-net-01.location
-  resource_group_name = azurerm_resource_group.rg-uks-hub-net-01.name
+  location            = azurerm_resource_group.rg-uks-hub-01.location
+  resource_group_name = azurerm_resource_group.rg-uks-hub-01.name
   sku_name            = "Standard"
 }
 
