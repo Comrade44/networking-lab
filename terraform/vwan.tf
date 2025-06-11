8e62f49440a450db016de2d88842bbf2a0926459
Richard McGinn <richard.mcginn@blakyaks.com>
2025-06-11 14:25:11 +0100

    Update SKU

--- terraform/vwan.tf
+++ terraform/vwan.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "rg-uks-net-01" {
-  name = "rg-uks-net-01"
+  name     = "rg-uks-net-01"
   location = "uksouth"
 }
 
@@ -10,7 +10,8 @@ resource "azurerm_virtual_wan" "vwan-uks-net-01" {
 }
 
 resource "azurerm_virtual_hub" "vhub-uks-net-01" {
-  name = "vhub-uks-net-01"
-  location = azurerm_resource_group.rg-uks-net-01.location
+  name                = "vhub-uks-net-01"
+  location            = azurerm_resource_group.rg-uks-net-01.location
   resource_group_name = azurerm_resource_group.rg-uks-net-01.name
+  sku                 = "Standard"
 }
