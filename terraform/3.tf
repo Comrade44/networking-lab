509bc60fb5dd275bcc3e1266a3748a28997b9480
Richard McGinn <richard.mcginn@blakyaks.com>
2025-06-13 14:01:11 +0100

    Fix typo

--- terraform/3.tf
+++ terraform/3.tf
@@ -17,34 +17,34 @@ resource "azurerm_subnet" "vnet-uks-hub-01-snet-01" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-resource "azurerm_virtual_network_peering" "compb-uks-hub-uks" {
-  name                      = "compb-uks-hub-uks"
-  resource_group_name       = azurerm_resource_group.rg-uks-compb-net-01.name
-  virtual_network_name      = azurerm_virtual_network.vnet-uks-compb-01.name
+resource "azurerm_virtual_network_peering" "spokeb-uks-hub-uks" {
+  name                      = "spokeb-uks-hub-uks"
+  resource_group_name       = azurerm_resource_group.rg-uks-spokeb-net-01.name
+  virtual_network_name      = azurerm_virtual_network.vnet-uks-spokeb-01.name
   remote_virtual_network_id = azurerm_virtual_network.vnet-uks-hub-01.id
   allow_forwarded_traffic   = true
 }
 
-resource "azurerm_virtual_network_peering" "hub-uks-compb-uks" {
-  name                      = "hub-uks-compb-uks"
+resource "azurerm_virtual_network_peering" "hub-uks-spokeb-uks" {
+  name                      = "hub-uks-spokeb-uks"
   resource_group_name       = azurerm_resource_group.rg-uks-hub-01.name
   virtual_network_name      = azurerm_virtual_network.vnet-uks-hub-01.name
-  remote_virtual_network_id = azurerm_virtual_network.vnet-uks-compb-01.id
+  remote_virtual_network_id = azurerm_virtual_network.vnet-uks-spokeb-01.id
   allow_forwarded_traffic   = true
 }
 
-resource "azurerm_virtual_network_peering" "compa-uks-hub-uks" {
-  name                      = "compa-uks-hub-uks"
-  resource_group_name       = azurerm_resource_group.rg-uks-compa-net-01.name
-  virtual_network_name      = azurerm_virtual_network.vnet-uks-compa-01.name
+resource "azurerm_virtual_network_peering" "spokea-uks-hub-uks" {
+  name                      = "spokea-uks-hub-uks"
+  resource_group_name       = azurerm_resource_group.rg-uks-spokea-net-01.name
+  virtual_network_name      = azurerm_virtual_network.vnet-uks-spokea-01.name
   remote_virtual_network_id = azurerm_virtual_network.vnet-uks-hub-01.id
   allow_forwarded_traffic   = true
 }
 
-resource "azurerm_virtual_network_peering" "hub-uks-compa-uks" {
-  name                      = "hub-uks-compa-uks"
+resource "azurerm_virtual_network_peering" "hub-uks-spokea-uks" {
+  name                      = "hub-uks-spokea-uks"
   resource_group_name       = azurerm_resource_group.rg-uks-hub-01.name
   virtual_network_name      = azurerm_virtual_network.vnet-uks-hub-01.name
-  remote_virtual_network_id = azurerm_virtual_network.vnet-uks-compa-01.id
+  remote_virtual_network_id = azurerm_virtual_network.vnet-uks-spokea-01.id
   allow_forwarded_traffic   = true
 }
