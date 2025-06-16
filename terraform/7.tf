42f938ee0c41ebf52b735c99985cb1bfa2caa44b
Richard McGinn <richard.mcginn@blakyaks.com>
2025-06-16 13:52:57 +0100

    Fix SKU

--- terraform/7.tf
+++ terraform/7.tf
@@ -2,7 +2,7 @@ resource "azurerm_firewall" "azfw-uks-net-01" {
   name                = "azfw-uks-net-01"
   location            = azurerm_resource_group.rg-uks-net-01.location
   resource_group_name = azurerm_resource_group.rg-uks-net-01.name
-  sku_name            = "AZFW_VNet"
+  sku_name            = "AZFW_Hub"
   sku_tier            = "Standard"
   virtual_hub {
     virtual_hub_id = azurerm_virtual_hub.vhub-uks-01.id
